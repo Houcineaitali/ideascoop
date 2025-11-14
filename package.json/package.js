fcd0ac6446e5f5b5cd21a6e9c862733a3fb8a641
Houcine Ait Ali <105569368+Houcineaitali@users.noreply.github.com>
2025-11-14 13:18:27 +0100

    Add files via upload

---
+++ package.json/package.js
@@ -0,0 +1,19 @@
+{
+    
+  "name"; "ideascoop-backend",
+  "version";"1.0.0",
+  "main"; "server.js",
+  "type"; "commonjs",
+  "dependencies"; {
+    "express"; "^4.21.2",
+    "cors"; "^2.8.5",
+    "body-parser"; "^1.20.2",
+    "dotenv"; "^16.6.1",
+    "mongoose"; "^8.0.0",
+    "ffmpeg-static"; "^5.1.0",
+    "fluent-ffmpeg"; "^2.1.2"
+  };
+   "scripts"; {
+    "start"; "node server.js"
+  }
+}
